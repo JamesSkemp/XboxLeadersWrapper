b7bc434e6992ce7ef0958affe857a071101d1ab8
James Skemp <strivinglife@gmail.com>
2013-06-10 07:21:58 -0500

    Fixed inconsistency with other API wrapper.

--- XboxLeadersWrapper/Formats.cs
+++ XboxLeadersWrapper/Formats.cs
@@ -1,24 +1,21 @@
 ﻿namespace XboxLeadersWrapper
 {
-	public partial class XboxApi
+	/// <summary>
+	/// Format to request result in.
+	/// </summary>
+	public enum ResultFormat
 	{
 		/// <summary>
-		/// Format to request result in.
+		/// Request JSON data.
 		/// </summary>
-		public enum ResultFormat
-		{
-			/// <summary>
-			/// Request JSON data.
-			/// </summary>
-			Json,
-			/// <summary>
-			/// Request XML data.
-			/// </summary>
-			Xml,
-			/// <summary>
-			/// Request PHP data. Client will need to convert string to PHP.
-			/// </summary>
-			Php
-		}
+		Json,
+		/// <summary>
+		/// Request XML data.
+		/// </summary>
+		Xml,
+		/// <summary>
+		/// Request PHP data. Client will need to convert string to PHP.
+		/// </summary>
+		Php
 	}
 }
