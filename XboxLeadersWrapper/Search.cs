3addf73edb94e5eb73d97f7d674660e087a329c4
James Skemp <strivinglife@gmail.com>
2013-06-20 07:02:39 -0500

    Added missing search functionality.

---
+++ XboxLeadersWrapper/Search.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XboxLeadersWrapper
+{
+	public partial class XboxApi
+	{
+		/// <summary>
+		/// Get search results for a particular query.
+		/// </summary>
+		/// <param name="query">Word or phrase to search the Xbox Marketplace for.</param>
+		/// <returns>String, of which format, with the search results.</returns>
+		public string GetSearch(string query)
+		{
+			var parameters = string.Format("query={0}&region={1}", query, this.Region);
+			return ApiRequest("search", parameters);
+		}
+	}
+}
